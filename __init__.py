bc8f1621b6495fadb62d9c36f97fd52b76af4d94
twined quartz <fmorisato@gmail.com>
2019-06-02 17:02:31 +0900

    save_plot

--- __init__.py
+++ __init__.py
@@ -3,3 +3,4 @@ from .subplot import Subplot
 from .subplot_time import SubplotTime
 from .layout import Layout
 from .figure_sizing import FigureSizing
+from .save_plot import save_plot
---
+++ save_plot.py
@@ -0,0 +1,32 @@
+import os
+import re
+import matplotlib.pyplot as plt
+
+
+def __safe_filename(filename: str) -> str:
+    return filename.replace("/", "").replace("\\", "")
+
+
+def __mkdir(f):
+    def wrapper(directory, *arg, **kwargs):
+        if not os.path.isdir(directory):
+            os.makedirs(directory)
+        return f(directory, *arg, **kwargs)
+    return wrapper
+
+
+def __actionSavePNG(directory, filename):
+    # パスが有効かどうかを検証し, ディレクトリがなければ作成する
+    return lambda postfix="": plt.savefig(directory+__safe_filename(filename+postfix+'.png'))
+
+
+def __IFigureSaver(ext):
+    if re.match(r"[pP](ng|NG)$", ext):
+        print("save as png")
+        return __actionSavePNG
+
+
+@__mkdir
+def save_plot(directory, fileName, ext="png"):
+    saver = __IFigureSaver(ext)
+    return saver(directory, fileName)()
